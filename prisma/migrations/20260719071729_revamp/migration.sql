311847748515dc0f2a2cb60ee15f031bc8bc33af
Sruthi Sami <sheisruthisami@gmail.com>
2026-07-20 13:23:30 +0530

    chore : added triggers for append only audits

--- prisma/migrations/20260719071729_revamp/migration.sql
+++ prisma/migrations/20260719071729_revamp/migration.sql
@@ -12,3 +12,20 @@ ALTER TABLE "AuditEvent" DROP COLUMN "metadata";
 -- AlterTable
 ALTER TABLE "User" DROP COLUMN "createdAt",
 DROP COLUMN "updatedAt";
+
+-- Prevent modification of audit events
+
+CREATE OR REPLACE FUNCTION prevent_audit_update()
+RETURNS TRIGGER AS
+$$
+BEGIN
+    RAISE EXCEPTION 'Audit events cannot be updated.';
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER audit_event_no_update
+BEFORE UPDATE
+ON "AuditEvent"
+FOR EACH ROW
+EXECUTE FUNCTION prevent_audit_update();
